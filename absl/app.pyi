3074b2e2f947eac96bcdc9388b65682f855f97f7
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-09-10 22:39:23 +0000

    Update reno for stable/pike

--- absl/app.pyi
+++ absl/app.pyi
@@ -4,4 +4,17 @@ from typing import Any, Callable, Collection, Iterable, List, NoReturn, Optional
 from absl.flags import _flag
 
 
-_MainArgs = TypeVar('_MainArgs'
+_MainArgs = TypeVar('_MainArgs')
+_Exc = TypeVar('_Exc', bound=Exception)
+
+
+class ExceptionHandler():
+
+  def wants(self, exc: _Exc) -> bool:
+    ...
+
+  def handle(self, exc: _Exc):
+    ...
+
+
+EXCEPTION_HANDLERS: List[Exc
