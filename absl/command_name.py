90bec0db2a4869d5e57bc51bd4d33beac7c7e2a0
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-09-20 06:23:42 +0000

    Fixing travis build

--- absl/command_name.py
+++ absl/command_name.py
@@ -1,1 +1,7 @@
-# Copyrigh
+# Copyright 2017 The Abseil Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  
