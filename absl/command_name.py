6714590dbde80fc0fb81943b8753fa7d351c00d6
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-09-20 18:27:44 +0000

    Added missing comments.

--- absl/command_name.py
+++ absl/command_name.py
@@ -8,4 +8,8 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS O
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A tiny stand alone library to change the kernel proce
