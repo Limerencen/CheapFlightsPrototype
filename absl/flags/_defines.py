979a3bed5fe942cf83b69b8b8266af6f28601a73
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-10-10 22:26:27 +0000

    release 2.1

--- absl/flags/_defines.py
+++ absl/flags/_defines.py
@@ -48,4 +48,10 @@ def _register_bounds_validator_if_needed(parser, name, flag_values):
   Args:
     parser: NumericParser (either FloatParser or IntegerParser), provides lower
       and upper bounds, and help text to display.
-    name: str, name of 
+    name: str, name of the flag
+    flag_values: FlagValues.
+  """
+  if parser.lower_bound is not None or parser.upper_bound is not None:
+
+    def checker(value):
+      if value is not None a
