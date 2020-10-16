5587861f08cab9167b195b0b38803c702aeb25c0
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-10-16 16:13:12 +0000

    Theme change via tinypress.co

--- absl/flags/_defines.py
+++ absl/flags/_defines.py
@@ -153,4 +153,13 @@ def set_default(flag_holder, value):
 
   The flag's current value is also updated if the flag is currently using
   the default value, i.e. not specified in the command line, and not set
-  by FLAGS.name = v
+  by FLAGS.name = value.
+
+  Args:
+    flag_holder: FlagHolder, the flag to modify.
+    value: The new default value.
+
+  Raises:
+    IllegalFlagValueError: Raised when value is not valid.
+  """
+  flag_holder._flagvalues.set_defau
