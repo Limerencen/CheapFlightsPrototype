85a3f6472708853ffd2f31d7290953989f0ebf00
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-10-31 10:19:27 +0000

    fix help text

--- absl/flags/_defines.py
+++ absl/flags/_defines.py
@@ -410,4 +410,7 @@ def DEFINE_integer(  # pylint: disable=invalid-name,redefined-builtin
     default: int|str|None, the default value of the flag.
     help: str, the help message.
     lower_bound: int, min value of the flag.
-    upper_bound: int, max 
+    upper_bound: int, max value of the flag.
+    flag_values: :class:`FlagValues`, the FlagValues instance with which the
+      flag will be registered. This should almost never need to be overridden.
+    required: bool, is this a required flag. Th
