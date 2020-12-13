a5fdf539cb6a4fd585560b1d628b58a9c8a420aa
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-12-13 17:16:33 +0000

    Adding test case

--- absl/flags/_validators.py
+++ absl/flags/_validators.py
@@ -56,4 +56,8 @@ def register_validator(flag_name,
     checker: callable, a function to validate the flag.
 
         * input - A single positional argument: The value of the corresponding
-          flag (string, boolean, etc.  This value will be passed to
+          flag (string, boolean, etc.  This value will be passed to checker
+          by the library).
+        * output - bool, True if validator constraint is satisfied.
+          If constraint is not satisfied, it should either ``return False`` or
+          ``raise flag
