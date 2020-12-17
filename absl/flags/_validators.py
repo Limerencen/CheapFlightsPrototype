c03b5951349b6b4c1292d3670ca8bd6991697494
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-12-17 22:01:32 +0000

    Update Rust

--- absl/flags/_validators.py
+++ absl/flags/_validators.py
@@ -145,4 +145,9 @@ def register_multi_flags_validator(flag_names,
     ValueError: Raised when multiple FlagValues are used in the same
         invocation. This can occur when FlagHolders have different `_flagvalues`
         or when str-type flag_names entries are present and the `flag_values`
-        argument does not match that of provided FlagHolder(s
+        argument does not match that of provided FlagHolder(s).
+  """
+  flag_names, flag_values = _flagvalues.resolve_flag_refs(
+      flag_names, flag_values)
+  v = _validators_classes.MultiFlagsValidator(
+      flag_names, multi_flags_checker
