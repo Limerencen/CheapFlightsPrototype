80a23ae3610bf96e23af6ca272c6b0212a386d43
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-12-23 16:03:23 +0000

    modified css

--- absl/flags/_validators.py
+++ absl/flags/_validators.py
@@ -243,4 +243,9 @@ def mark_flags_as_required(flag_names, flag_values=_flagvalues.FLAGS):
         flags.mark_flags_as_required(['flag1', 'flag2', 'flag3'])
         app.run()
 
-  A
+  Args:
+    flag_names: Sequence[str | FlagHolder], names or holders of the flags.
+    flag_values: flags.FlagValues, optional FlagValues instance where the flags
+        are defined.
+  Raises:
+    AttributeError: 
