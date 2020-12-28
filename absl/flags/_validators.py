9f867a5b7924792aec0060005d6676f6e48f4620
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-12-28 22:36:14 +0000

    commit at `date`

--- absl/flags/_validators.py
+++ absl/flags/_validators.py
@@ -310,3 +310,9 @@ def mark_bool_flags_as_mutual_exclusive(flag_names, required=False,
         Positional-only parameter.
     required: bool. If true, exactly one flag must be True. Otherwise, at most
         one flag can be True, and it is valid for all flags to be False.
+    flag_values: flags.FlagValues, optional FlagValues instance where the flags
+        are defined.
+
+  Raises:
+    ValueError: Raised when multiple FlagValues are used in the same
+        invocation. T
