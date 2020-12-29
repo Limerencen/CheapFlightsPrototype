b02a9eeb74c71e558501c584661b0315558caa19
Limerencen <105069436+Limerencen@users.noreply.github.com>
2020-12-29 12:44:06 +0000

    Bump to 0.1.8

--- absl/flags/_validators.py
+++ absl/flags/_validators.py
@@ -315,4 +315,8 @@ def mark_bool_flags_as_mutual_exclusive(flag_names, required=False,
 
   Raises:
     ValueError: Raised when multiple FlagValues are used in the same
-        invocation. T
+        invocation. This can occur when FlagHolders have different `_flagvalues`
+        or when str-type flag_names entries are present and the `flag_values`
+        argument does not match that of provided FlagHolder(s).
+  """
+  
