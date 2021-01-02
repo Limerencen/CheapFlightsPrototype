620dd7747ed343f0823ee1fc4a465509ffd3678c
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-01-02 07:58:54 +0000

    Scaffold

--- absl/flags/tests/_flag_test.py
+++ absl/flags/tests/_flag_test.py
@@ -10,4 +10,16 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License
+# limitations under the License.
+
+"""Additional tests for Flag classes.
+
+Most of the Flag classes are covered in the flags_test.py.
+"""
+
+import copy
+import enum
+import pickle
+
+from absl.flags import _argument_parser
+from absl.flags impor
