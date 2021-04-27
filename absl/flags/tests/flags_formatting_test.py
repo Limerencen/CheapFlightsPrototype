4fdb3e0decf9381f860b0e98a44990817cee2f10
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-04-27 12:28:40 +0000

    update language

--- absl/flags/tests/flags_formatting_test.py
+++ absl/flags/tests/flags_formatting_test.py
@@ -98,3 +98,117 @@ class FlagsUnitTest(absltest.TestCase):
     # We respect prefix
     self.assertEqual(' a\n b\n c', flags.text_wrap('a\nb\nc', 80, ' '))
     self.assertEqual('a\n b\n c', flags.text_wrap('a\nb\nc', 80, ' ', ''))
+
+    # tabs
+    self.assertEqual('a\n b   c',
+                     flags.text_wrap('a\nb\tc', 80, ' ', ''))
+    self.assertEqual('a\n bb  c',
+                     flags.text_wrap('a\nbb\tc', 80, ' ', ''))
+    self.assertEqual('a\n bbb c',
+                     flags.text_wrap('a\nbbb\tc', 80, ' ', ''))
+    self.assertEqual('a\n bbbb    c',
+                     flags.text_wrap('a\nbbbb\tc', 80, ' ', ''))
+    self.assertEqual('a\n b\n c\n d',
+                     flags.text_wrap('a\nb\tc\td', 3, ' ', ''))
+    self.assertEqual('a\n b\n c\n d',
+                     flags.text_wrap('a\nb\tc\td', 4, ' ', ''))
+    self.assertEqual('a\n b\n c\n d',
+                     flags.text_wrap('a\nb\tc\td', 5, ' ', ''))
+    self.assertEqual('a\n b   c\n d',
+                     flags.text_wrap('a\nb\tc\td', 6, ' ', ''))
+    self.assertEqual('a\n b   c\n d',
+                     flags.text_wrap('a\nb\tc\td', 7, ' ', ''))
+    self.assertEqual('a\n b   c\n d',
+                     flags.text_wrap('a\nb\tc\td', 8, ' ', ''))
+    self.assertEqual('a\n b   c\n d',
+                     flags.text_wrap('a\nb\tc\td', 9, ' ', ''))
+    self.assertEqual('a\n b   c   d',
+                     flags.text_wrap('a\nb\tc\td', 10, ' ', ''))
+
+    # multiple tabs
+    self.assertEqual('a       c',
+                     flags.text_wrap('a\t\tc', 80, ' ', ''))
+
+    _helpers._DEFAULT_HELP_WIDTH = default_help_width  # restore
+
+  def test_doc_to_help(self):
+    self.assertEqual('', flags.doc_to_help('  '))
+    self.assertEqual('', flags.doc_to_help('  \n  '))
+    self.assertEqual('a\n\nb', flags.doc_to_help('a\n  \nb'))
+    self.assertEqual('a\n\n\nb', flags.doc_to_help('a\n  \n  \nb'))
+    self.assertEqual('a b', flags.doc_to_help('  a\nb  '))
+    self.assertEqual('a b', flags.doc_to_help('\na\nb\n'))
+    self.assertEqual('a\n\nb', flags.doc_to_help('\na\n\nb\n'))
+    self.assertEqual('a b', flags.doc_to_help('  \na\nb\n  '))
+    # Different first line, one line empty - erm double new line.
+    self.assertEqual('a b c\n\nd', flags.doc_to_help('a\n  b\n  c\n\n  d'))
+    self.assertEqual('a b\n      c d', flags.doc_to_help('a\n  b\n  \tc\n  d'))
+    self.assertEqual('a b\n      c\n      d',
+                     flags.doc_to_help('a\n  b\n  \tc\n  \td'))
+
+  def test_doc_to_help_flag_values(self):
+    # !!!!!!!!!!!!!!!!!!!!
+    # The following doc string is taken as is directly from flags.py:FlagValues
+    # The intention of this test is to verify 'live' performance
+    # !!!!!!!!!!!!!!!!!!!!
+    """Used as a registry for 'Flag' objects.
+
+    A 'FlagValues' can then scan command line arguments, passing flag
+    arguments through to the 'Flag' objects that it owns.  It also
+    provides easy access to the flag values.  Typically only one
+    'FlagValues' object is needed by an application:  flags.FLAGS
+
+    This class is heavily overloaded:
+
+    'Flag' objects are registered via __setitem__:
+         FLAGS['longname'] = x   # register a new flag
+
+    The .value member of the registered 'Flag' objects can be accessed as
+    members of this 'FlagValues' object, through __getattr__.  Both the
+    long and short name of the original 'Flag' objects can be used to
+    access its value:
+         FLAGS.longname          # parsed flag value
+         FLAGS.x                 # parsed flag value (short name)
+
+    Command line arguments are scanned and passed to the registered 'Flag'
+    objects through the __call__ method.  Unparsed arguments, including
+    argv[0] (e.g. the program name) are returned.
+         argv = FLAGS(sys.argv)  # scan command line arguments
+
+    The original registered Flag objects can be retrieved through the use
+    """
+    doc = flags.doc_to_help(self.test_doc_to_help_flag_values.__doc__)
+    # Test the general outline of the converted docs
+    lines = doc.splitlines()
+    self.assertEqual(17, len(lines))
+    empty_lines = [index for index in range(len(lines)) if not lines[index]]
+    self.assertEqual([1, 3, 5, 8, 12, 15], empty_lines)
+    # test that some starting prefix is kept
+    flags_lines = [index for index in range(len(lines))
+                   if lines[index].startswith('     FLAGS')]
+    self.assertEqual([7, 10, 11], flags_lines)
+    # but other, especially common space has been removed
+    space_lines = [index for index in range(len(lines))
+                   if lines[index] and lines[index][0].isspace()]
+    self.assertEqual([7, 10, 11, 14], space_lines)
+    # No right space was kept
+    rspace_lines = [index for index in range(len(lines))
+                    if lines[index] != lines[index].rstrip()]
+    self.assertEqual([], rspace_lines)
+    # test double spaces are kept
+    self.assertEqual(True, lines[2].endswith('application:  flags.FLAGS'))
+
+  def test_text_wrap_raises_on_excessive_indent(self):
+    """Ensure an indent longer than line length raises."""
+    self.assertRaises(ValueError,
+                      flags.text_wrap, 'dummy', length=10, indent=' ' * 10)
+
+  def test_text_wrap_raises_on_excessive_first_line(self):
+    """Ensure a first line indent longer than line length raises."""
+    self.assertRaises(
+        ValueError,
+        flags.text_wrap, 'dummy', length=80, firstline_indent=' ' * 80)
+
+
+if __name__ == '__main__':
+  absltest.main()
