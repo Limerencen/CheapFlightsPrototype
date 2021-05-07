e78625551bad67b74225aa3487394013ecf4479f
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-05-07 06:35:53 +0000

    add missing function

--- absl/flags/tests/flags_unicode_literals_test.py
+++ absl/flags/tests/flags_unicode_literals_test.py
@@ -9,4 +9,10 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the Lic
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test the use of flags when from __future__ import unicode_literals is on."""
+
+from absl import flags
+from absl
