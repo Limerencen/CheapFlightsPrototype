ab63175ab5d682b22d4d111ed386321c8176c58c
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-06-10 07:23:53 +0000

    Updated feature list

--- absl/logging/tests/logging_functional_test_helper.py
+++ absl/logging/tests/logging_functional_test_helper.py
@@ -9,4 +9,12 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language govern
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Helper script for logging_functional_test."""
+
+import logging as std_logging
+import logging.config as std_logging_config
+import os
+import sys
