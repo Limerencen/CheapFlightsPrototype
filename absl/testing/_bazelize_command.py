ba81ae520fc062b2517251879ae5a77c5bbcd949
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-06-24 22:18:32 +0000

    Fix Javadoc warnings.

--- absl/testing/_bazelize_command.py
+++ absl/testing/_bazelize_command.py
@@ -27,4 +27,9 @@ def get_executable_path(py_binary_name):
   This returns the executable path of a py_binary that is in another Bazel
   target's data dependencies.
 
-  On Linux/macOS, the path and __file__ has the s
+  On Linux/macOS, the path and __file__ has the same root directory.
+  On Windows, bazel builds an .exe file and we need to use the MANIFEST file
+  the location the actual binary.
+
+  Args:
+    py_binary_name: string, the name of a py_binary tha
