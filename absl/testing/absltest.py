5b9965a80d81f5e24a86cc4779fa4af13c951c73
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-07-01 09:16:38 +0000

    users ui test

--- absl/testing/absltest.py
+++ absl/testing/absltest.py
@@ -126,3 +126,218 @@ def expectedFailureIf(condition, reason):  # pylint: disable=invalid-name
 class TempFileCleanup(enum.Enum):
   # Always cleanup temp files when the test completes.
   ALWAYS = 'always'
+  # Only cleanup temp file if the test passes. This allows easier inspection
+  # of tempfile contents on test failure. absltest.TEST_TMPDIR.value determines
+  # where tempfiles are created.
+  SUCCESS = 'success'
+  # Never cleanup temp files.
+  OFF = 'never'
+
+
+# Many of the methods in this module have names like assertSameElements.
+# This kind of name does not comply with PEP8 style,
+# but it is consistent with the naming of methods in unittest.py.
+# pylint: disable=invalid-name
+
+
+def _get_default_test_random_seed():
+  # type: () -> int
+  random_seed = 301
+  value = os.environ.get('TEST_RANDOM_SEED', '')
+  try:
+    random_seed = int(value)
+  except ValueError:
+    pass
+  return random_seed
+
+
+def get_default_test_srcdir():
+  # type: () -> Text
+  """Returns default test source dir."""
+  return os.environ.get('TEST_SRCDIR', '')
+
+
+def get_default_test_tmpdir():
+  # type: () -> Text
+  """Returns default test temp dir."""
+  tmpdir = os.environ.get('TEST_TMPDIR', '')
+  if not tmpdir:
+    tmpdir = os.path.join(tempfile.gettempdir(), 'absl_testing')
+
+  return tmpdir
+
+
+def _get_default_randomize_ordering_seed():
+  # type: () -> int
+  """Returns default seed to use for randomizing test order.
+
+  This function first checks the --test_randomize_ordering_seed flag, and then
+  the TEST_RANDOMIZE_ORDERING_SEED environment variable. If the first value
+  we find is:
+    * (not set): disable test randomization
+    * 0: disable test randomization
+    * 'random': choose a random seed in [1, 4294967295] for test order
+      randomization
+    * positive integer: use this seed for test order randomization
+
+  (The values used are patterned after
+  https://docs.python.org/3/using/cmdline.html#envvar-PYTHONHASHSEED).
+
+  In principle, it would be simpler to return None if no override is provided;
+  however, the python random module has no `get_seed()`, only `getstate()`,
+  which returns far more data than we want to pass via an environment variable
+  or flag.
+
+  Returns:
+    A default value for test case randomization (int). 0 means do not randomize.
+
+  Raises:
+    ValueError: Raised when the flag or env value is not one of the options
+        above.
+  """
+  if FLAGS['test_randomize_ordering_seed'].present:
+    randomize = FLAGS.test_randomize_ordering_seed
+  elif 'TEST_RANDOMIZE_ORDERING_SEED' in os.environ:
+    randomize = os.environ['TEST_RANDOMIZE_ORDERING_SEED']
+  else:
+    randomize = ''
+  if not randomize:
+    return 0
+  if randomize == 'random':
+    return random.Random().randint(1, 4294967295)
+  if randomize == '0':
+    return 0
+  try:
+    seed = int(randomize)
+    if seed > 0:
+      return seed
+  except ValueError:
+    pass
+  raise ValueError(
+      'Unknown test randomization seed value: {}'.format(randomize))
+
+
+TEST_SRCDIR = flags.DEFINE_string(
+    'test_srcdir',
+    get_default_test_srcdir(),
+    'Root of directory tree where source files live',
+    allow_override_cpp=True)
+TEST_TMPDIR = flags.DEFINE_string(
+    'test_tmpdir',
+    get_default_test_tmpdir(),
+    'Directory for temporary testing files',
+    allow_override_cpp=True)
+
+flags.DEFINE_integer(
+    'test_random_seed',
+    _get_default_test_random_seed(),
+    'Random seed for testing. Some test frameworks may '
+    'change the default value of this flag between runs, so '
+    'it is not appropriate for seeding probabilistic tests.',
+    allow_override_cpp=True)
+flags.DEFINE_string(
+    'test_randomize_ordering_seed',
+    '',
+    'If positive, use this as a seed to randomize the '
+    'execution order for test cases. If "random", pick a '
+    'random seed to use. If 0 or not set, do not randomize '
+    'test case execution order. This flag also overrides '
+    'the TEST_RANDOMIZE_ORDERING_SEED environment variable.',
+    allow_override_cpp=True)
+flags.DEFINE_string('xml_output_file', '', 'File to store XML test results')
+
+
+# We might need to monkey-patch TestResult so that it stops considering an
+# unexpected pass as a as a "successful result".  For details, see
+# http://bugs.python.org/issue20165
+def _monkey_patch_test_result_for_unexpected_passes():
+  # type: () -> None
+  """Workaround for <http://bugs.python.org/issue20165>."""
+
+  def wasSuccessful(self):
+    # type: () -> bool
+    """Tells whether or not this result was a success.
+
+    Any unexpected pass is to be counted as a non-success.
+
+    Args:
+      self: The TestResult instance.
+
+    Returns:
+      Whether or not this result was a success.
+    """
+    return (len(self.failures) == len(self.errors) ==
+            len(self.unexpectedSuccesses) == 0)
+
+  test_result = unittest.TestResult()
+  test_result.addUnexpectedSuccess(unittest.FunctionTestCase(lambda: None))
+  if test_result.wasSuccessful():  # The bug is present.
+    unittest.TestResult.wasSuccessful = wasSuccessful
+    if test_result.wasSuccessful():  # Warn the user if our hot-fix failed.
+      sys.stderr.write('unittest.result.TestResult monkey patch to report'
+                       ' unexpected passes as failures did not work.\n')
+
+
+_monkey_patch_test_result_for_unexpected_passes()
+
+
+def _open(filepath, mode, _open_func=open):
+  # type: (Text, Text, Callable[..., IO]) -> IO
+  """Opens a file.
+
+  Like open(), but ensure that we can open real files even if tests stub out
+  open().
+
+  Args:
+    filepath: A filepath.
+    mode: A mode.
+    _open_func: A built-in open() function.
+
+  Returns:
+    The opened file object.
+  """
+  return _open_func(filepath, mode, encoding='utf-8')
+
+
+class _TempDir(object):
+  """Represents a temporary directory for tests.
+
+  Creation of this class is internal. Using its public methods is OK.
+
+  This class implements the `os.PathLike` interface (specifically,
+  `os.PathLike[str]`). This means, in Python 3, it can be directly passed
+  to e.g. `os.path.join()`.
+  """
+
+  def __init__(self, path):
+    # type: (Text) -> None
+    """Module-private: do not instantiate outside module."""
+    self._path = path
+
+  @property
+  def full_path(self):
+    # type: () -> Text
+    """Returns the path, as a string, for the directory.
+
+    TIP: Instead of e.g. `os.path.join(temp_dir.full_path)`, you can simply
+    do `os.path.join(temp_dir)` because `__fspath__()` is implemented.
+    """
+    return self._path
+
+  def __fspath__(self):
+    # type: () -> Text
+    """See os.PathLike."""
+    return self.full_path
+
+  def create_file(self, file_path=None, content=None, mode='w', encoding='utf8',
+                  errors='strict'):
+    # type: (Optional[Text], Optional[AnyStr], Text, Text, Text) -> _TempFile
+    """Create a file in the directory.
+
+    NOTE: If the file already exists, it will be made writable and overwritten.
+
+    Args:
+      file_path: Optional file path for the temp file. If not given, a unique
+        file name will be generated and used. Slashes are allowed in the name;
+        any missing intermediate directories will be created. NOTE: This path
+        is the path that will be cleaned up, including any directories in the
