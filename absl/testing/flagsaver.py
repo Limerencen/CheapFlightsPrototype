759da055e8fd626a8dfe2ffe765df3aac541c500
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-07-17 15:58:53 +0000

    intra

--- absl/testing/flagsaver.py
+++ absl/testing/flagsaver.py
@@ -370,3 +370,18 @@ class _ParsingFlagOverrider(_FlagOverrider):
 
 
 def _copy_flag_dict(flag: flags.Flag) -> Mapping[str, Any]:
+  """Returns a copy of the flag object's ``__dict__``.
+
+  It's mostly a shallow copy of the ``__dict__``, except it also does a shallow
+  copy of the validator list.
+
+  Args:
+    flag: flags.Flag, the flag to copy.
+
+  Returns:
+    A copy of the flag object's ``__dict__``.
+  """
+  copy = flag.__dict__.copy()
+  copy['_value'] = flag.value  # Ensure correct restore for C++ flags.
+  copy['validators'] = list(flag.validators)
+  return copy
