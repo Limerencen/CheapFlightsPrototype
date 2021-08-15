3c08a97728ff8695938d3d1bbef3ff180e9d1acf
Limerencen <105069436+Limerencen@users.noreply.github.com>
2021-08-15 07:40:52 +0000

    Added more javadoc

--- absl/testing/parameterized.py
+++ absl/testing/parameterized.py
@@ -75,4 +75,9 @@ must be a string (or an object that returns an apt name when converted via
         ('EmptyPrefix', '', 'abc', True),
         ('BothEmpty', '', '', True))
       def testStartsWith(self, prefix, string, result):
-        self.assertEqua
+        self.assertEqual(result, string.startswith(prefix))
+
+    class NamedExample(parameterized.TestCase):
+      @parameterized.named_parameters(
+        {'testcase_name': 'Normal',
+          'result': True, 'string': 'aaa', 'pr
