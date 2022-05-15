893e0b0f0603dbdd0d7d54e91988ca9bdbbf4399
Limerencen <105069436+Limerencen@users.noreply.github.com>
2022-05-15 22:39:39 +0000

    basic docs

--- absl/testing/tests/absltest_py3_test.py
+++ absl/testing/tests/absltest_py3_test.py
@@ -8,4 +8,12 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Python3-only Tests for absltest."""
+
+from absl.testing import absltest
+
+
+class GetTestCaseNamesPEP3102Te
