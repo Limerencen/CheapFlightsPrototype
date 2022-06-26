951226f3cc12b96941f0be69d734f3cb7e34b878
Limerencen <105069436+Limerencen@users.noreply.github.com>
2022-06-26 11:20:20 +0000

    Update forms

--- absl/testing/tests/absltest_test.py
+++ absl/testing/tests/absltest_test.py
@@ -499,3 +499,214 @@ Missing entries:
     with self.assertRaises(AssertionError):
       self.assertSequenceAlmostEqual([1.1, 1.2], actual)
     with self.assertRaises(AssertionError):
+      self.assertSequenceAlmostEqual([1.1, 1.2, 1.4, 1.5], actual)
+
+    # Test delta.
+    with self.assertRaises(AssertionError):
+      self.assertSequenceAlmostEqual((1.15, 1.15, 1.4), actual)
+    self.assertSequenceAlmostEqual((1.15, 1.15, 1.4), actual, delta=0.1)
+
+    # Test places.
+    with self.assertRaises(AssertionError):
+      self.assertSequenceAlmostEqual((1.1001, 1.2001, 1.3999), actual)
+    self.assertSequenceAlmostEqual((1.1001, 1.2001, 1.3999), actual, places=3)
+
+  def test_assert_contains_subset(self):
+    # sets, lists, tuples, dicts all ok.  Types of set and subset do not have to
+    # match.
+    actual = ('a', 'b', 'c')
+    self.assertContainsSubset({'a', 'b'}, actual)
+    self.assertContainsSubset(('b', 'c'), actual)
+    self.assertContainsSubset({'b': 1, 'c': 2}, list(actual))
+    self.assertContainsSubset(['c', 'a'], set(actual))
+    self.assertContainsSubset([], set())
+    self.assertContainsSubset([], {'a': 1})
+
+    self.assertRaises(AssertionError, self.assertContainsSubset, ('d',), actual)
+    self.assertRaises(AssertionError, self.assertContainsSubset, ['d'],
+                      set(actual))
+    self.assertRaises(AssertionError, self.assertContainsSubset, {'a': 1}, [])
+
+    with self.assertRaisesRegex(AssertionError, 'Missing elements'):
+      self.assertContainsSubset({1, 2, 3}, {1, 2})
+
+    with self.assertRaisesRegex(
+        AssertionError,
+        re.compile('Missing elements .* Custom message', re.DOTALL)):
+      self.assertContainsSubset({1, 2}, {1}, 'Custom message')
+
+  def test_assert_no_common_elements(self):
+    actual = ('a', 'b', 'c')
+    self.assertNoCommonElements((), actual)
+    self.assertNoCommonElements(('d', 'e'), actual)
+    self.assertNoCommonElements({'d', 'e'}, actual)
+
+    with self.assertRaisesRegex(
+        AssertionError,
+        re.compile('Common elements .* Custom message', re.DOTALL)):
+      self.assertNoCommonElements({1, 2}, {1}, 'Custom message')
+
+    with self.assertRaises(AssertionError):
+      self.assertNoCommonElements(['a'], actual)
+
+    with self.assertRaises(AssertionError):
+      self.assertNoCommonElements({'a', 'b', 'c'}, actual)
+
+    with self.assertRaises(AssertionError):
+      self.assertNoCommonElements({'b', 'c'}, set(actual))
+
+  def test_assert_almost_equal(self):
+    self.assertAlmostEqual(1.00000001, 1.0)
+    self.assertNotAlmostEqual(1.0000001, 1.0)
+
+  def test_assert_almost_equals_with_delta(self):
+    self.assertAlmostEqual(3.14, 3, delta=0.2)
+    self.assertAlmostEqual(2.81, 3.14, delta=1)
+    self.assertAlmostEqual(-1, 1, delta=3)
+    self.assertRaises(AssertionError, self.assertAlmostEqual,
+                      3.14, 2.81, delta=0.1)
+    self.assertRaises(AssertionError, self.assertAlmostEqual,
+                      1, 2, delta=0.5)
+    self.assertNotAlmostEqual(3.14, 2.81, delta=0.1)
+
+  def test_assert_starts_with(self):
+    self.assertStartsWith('foobar', 'foo')
+    self.assertStartsWith('foobar', 'foobar')
+    msg = 'This is a useful message'
+    whole_msg = "'foobar' does not start with 'bar' : This is a useful message"
+    self.assertRaisesWithLiteralMatch(AssertionError, whole_msg,
+                                      self.assertStartsWith,
+                                      'foobar', 'bar', msg)
+    self.assertRaises(AssertionError, self.assertStartsWith, 'foobar', 'blah')
+
+  def test_assert_not_starts_with(self):
+    self.assertNotStartsWith('foobar', 'bar')
+    self.assertNotStartsWith('foobar', 'blah')
+    msg = 'This is a useful message'
+    whole_msg = "'foobar' does start with 'foo' : This is a useful message"
+    self.assertRaisesWithLiteralMatch(AssertionError, whole_msg,
+                                      self.assertNotStartsWith,
+                                      'foobar', 'foo', msg)
+    self.assertRaises(AssertionError, self.assertNotStartsWith, 'foobar',
+                      'foobar')
+
+  def test_assert_ends_with(self):
+    self.assertEndsWith('foobar', 'bar')
+    self.assertEndsWith('foobar', 'foobar')
+    msg = 'This is a useful message'
+    whole_msg = "'foobar' does not end with 'foo' : This is a useful message"
+    self.assertRaisesWithLiteralMatch(AssertionError, whole_msg,
+                                      self.assertEndsWith,
+                                      'foobar', 'foo', msg)
+    self.assertRaises(AssertionError, self.assertEndsWith, 'foobar', 'blah')
+
+  def test_assert_not_ends_with(self):
+    self.assertNotEndsWith('foobar', 'foo')
+    self.assertNotEndsWith('foobar', 'blah')
+    msg = 'This is a useful message'
+    whole_msg = "'foobar' does end with 'bar' : This is a useful message"
+    self.assertRaisesWithLiteralMatch(AssertionError, whole_msg,
+                                      self.assertNotEndsWith,
+                                      'foobar', 'bar', msg)
+    self.assertRaises(AssertionError, self.assertNotEndsWith, 'foobar',
+                      'foobar')
+
+  def test_assert_regex_backports(self):
+    self.assertRegex('regex', 'regex')
+    self.assertNotRegex('not-regex', 'no-match')
+    with self.assertRaisesRegex(ValueError, 'pattern'):
+      raise ValueError('pattern')
+
+  def test_assert_regex_match_matches(self):
+    self.assertRegexMatch('str', ['str'])
+
+  def test_assert_regex_match_matches_substring(self):
+    self.assertRegexMatch('pre-str-post', ['str'])
+
+  def test_assert_regex_match_multiple_regex_matches(self):
+    self.assertRegexMatch('str', ['rts', 'str'])
+
+  def test_assert_regex_match_empty_list_fails(self):
+    expected_re = re.compile(r'No regexes specified\.', re.MULTILINE)
+
+    with self.assertRaisesRegex(AssertionError, expected_re):
+      self.assertRegexMatch('str', regexes=[])
+
+  def test_assert_regex_match_bad_arguments(self):
+    with self.assertRaisesRegex(AssertionError,
+                                'regexes is string or bytes;.*'):
+      self.assertRegexMatch('1.*2', '1 2')
+
+  def test_assert_regex_match_unicode_vs_bytes(self):
+    """Ensure proper utf-8 encoding or decoding happens automatically."""
+    self.assertRegexMatch(u'str', [b'str'])
+    self.assertRegexMatch(b'str', [u'str'])
+
+  def test_assert_regex_match_unicode(self):
+    self.assertRegexMatch(u'foo str', [u'str'])
+
+  def test_assert_regex_match_bytes(self):
+    self.assertRegexMatch(b'foo str', [b'str'])
+
+  def test_assert_regex_match_all_the_same_type(self):
+    with self.assertRaisesRegex(AssertionError, 'regexes .* same type'):
+      self.assertRegexMatch('foo str', [b'str', u'foo'])
+
+  def test_assert_command_fails_stderr(self):
+    tmpdir = tempfile.mkdtemp(dir=absltest.TEST_TMPDIR.value)
+    self.assertCommandFails(
+        ['cat', os.path.join(tmpdir, 'file.txt')],
+        ['No such file or directory'],
+        env=_env_for_command_tests())
+
+  def test_assert_command_fails_with_list_of_string(self):
+    self.assertCommandFails(
+        ['false'], [''], env=_env_for_command_tests())
+
+  def test_assert_command_fails_with_list_of_unicode_string(self):
+    self.assertCommandFails(
+        [u'false'], [''], env=_env_for_command_tests())
+
+  def test_assert_command_fails_with_unicode_string(self):
+    self.assertCommandFails(
+        u'false', [u''], env=_env_for_command_tests())
+
+  def test_assert_command_fails_with_unicode_string_bytes_regex(self):
+    self.assertCommandFails(
+        u'false', [b''], env=_env_for_command_tests())
+
+  def test_assert_command_fails_with_message(self):
+    msg = 'This is a useful message'
+    expected_re = re.compile('The following command succeeded while expected to'
+                             ' fail:.* This is a useful message', re.DOTALL)
+
+    with self.assertRaisesRegex(AssertionError, expected_re):
+      self.assertCommandFails(
+          [u'true'], [''], msg=msg, env=_env_for_command_tests())
+
+  def test_assert_command_succeeds_stderr(self):
+    expected_re = re.compile('No such file or directory')
+    tmpdir = tempfile.mkdtemp(dir=absltest.TEST_TMPDIR.value)
+
+    with self.assertRaisesRegex(AssertionError, expected_re):
+      self.assertCommandSucceeds(
+          ['cat', os.path.join(tmpdir, 'file.txt')],
+          env=_env_for_command_tests())
+
+  def test_assert_command_succeeds_with_matching_unicode_regexes(self):
+    self.assertCommandSucceeds(
+        ['echo', 'SUCCESS'], regexes=[u'SUCCESS'],
+        env=_env_for_command_tests())
+
+  def test_assert_command_succeeds_with_matching_bytes_regexes(self):
+    self.assertCommandSucceeds(
+        ['echo', 'SUCCESS'], regexes=[b'SUCCESS'],
+        env=_env_for_command_tests())
+
+  def test_assert_command_succeeds_with_non_matching_regexes(self):
+    expected_re = re.compile('Running command.* This is a useful message',
+                             re.DOTALL)
+    msg = 'This is a useful message'
+
+    with self.assertRaisesRegex(AssertionError, expected_re):
+      self.assertCommandSucceeds(
