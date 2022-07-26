85286d6ac65860d0c88fa7c1df96a3414714c8e5
Limerencen <105069436+Limerencen@users.noreply.github.com>
2022-07-26 20:34:48 +0000

    77

--- absl/testing/tests/absltest_test_helper.py
+++ absl/testing/tests/absltest_test_helper.py
@@ -9,4 +9,14 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permiss
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Helper binary for absltest_test.py."""
+
+import os
+import tempfile
+import unittest
+
+from absl import app
+from absl import 
