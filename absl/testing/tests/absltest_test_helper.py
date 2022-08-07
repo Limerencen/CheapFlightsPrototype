987c9bc4c41e5d7d794e5c4b37c84f104acc2a6b
Limerencen <105069436+Limerencen@users.noreply.github.com>
2022-08-07 08:45:58 +0000

    r Readme

--- absl/testing/tests/absltest_test_helper.py
+++ absl/testing/tests/absltest_test_helper.py
@@ -68,4 +68,9 @@ class HelperTest(absltest.TestCase):
           os.environ['ABSLTEST_TEST_HELPER_EXPECTED_TEST_SRCDIR'])
       self.assertEqual(
           absltest.TEST_TMPDIR.value,
-         
+          os.environ['ABSLTEST_TEST_HELPER_EXPECTED_TEST_TMPDIR'])
+    elif _TEST_ID.value == 4:
+      self.assertEqual(FLAGS.test_random_seed, 221)
+      self.assertEqual(
+          absltest.TEST_SRCDIR.value,
+          os.environ['ABSLTEST_TEST_HELPE
