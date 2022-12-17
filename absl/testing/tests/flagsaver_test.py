66ac4fe2149d3f42a30397f46b68f01df72dbb85
Limerencen <105069436+Limerencen@users.noreply.github.com>
2022-12-17 17:17:50 +0000

    Bump rdoinfo queens-uc tags to latest upper-constraints

--- absl/testing/tests/flagsaver_test.py
+++ absl/testing/tests/flagsaver_test.py
@@ -520,3 +520,11 @@ class SetUpTearDownTest(absltest.TestCase):
 @parameterized.named_parameters(
     dict(
         testcase_name='flagsaver.flagsaver',
+        flagsaver_method=flagsaver.flagsaver,
+    ),
+    dict(
+        testcase_name='flagsaver.as_parsed',
+        flagsaver_method=flagsaver.as_parsed,
+    ),
+)
+class BadUsageTest(parame
