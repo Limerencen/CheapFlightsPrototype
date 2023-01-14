847980f87713bfea0d6cf913e589563b7969d90a
Limerencen <105069436+Limerencen@users.noreply.github.com>
2023-01-14 14:08:32 +0000

    Prepare next release

--- absl/testing/tests/parameterized_test.py
+++ absl/testing/tests/parameterized_test.py
@@ -336,3 +336,189 @@ class ParameterizedTestsTest(absltest.TestCase):
       pass
 
   class UniqueDescriptiveNamesTest(parameterized.TestCase):
+
+    @parameterized.parameters(13, 13)
+    def test_normal(self, number):
+      del number
+
+  class MultiGeneratorsTestCase(parameterized.TestCase):
+
+    @parameterized.parameters((i for i in (1, 2, 3)), (i for i in (3, 2, 1)))
+    def test_sum(self, a, b, c):
+      self.assertEqual(6, sum([a, b, c]))
+
+  class NamedParametersReusableTestCase(parameterized.TestCase):
+    named_params_a = (
+        {'testcase_name': 'dict_a', 'unused_obj': 0},
+        ('list_a', 1),
+    )
+    named_params_b = (
+        {'testcase_name': 'dict_b', 'unused_obj': 2},
+        ('list_b', 3),
+    )
+    named_params_c = (
+        {'testcase_name': 'dict_c', 'unused_obj': 4},
+        ('list_b', 5),
+    )
+
+    @parameterized.named_parameters(*(named_params_a + named_params_b))
+    def testSomething(self, unused_obj):
+      pass
+
+    @parameterized.named_parameters(*(named_params_a + named_params_c))
+    def testSomethingElse(self, unused_obj):
+      pass
+
+  class SuperclassTestCase(parameterized.TestCase):
+
+    @parameterized.parameters('foo', 'bar')
+    def test_name(self, name):
+      del name
+
+  class SubclassTestCase(SuperclassTestCase):
+    pass
+
+  @unittest.skipIf(
+      (sys.version_info[:2] == (3, 7) and sys.version_info[2] in {0, 1, 2}),
+      'Python 3.7.0 to 3.7.2 have a bug that breaks this test, see '
+      'https://bugs.python.org/issue35767')
+  def test_missing_inheritance(self):
+    ts = unittest.makeSuite(self.BadAdditionParams)
+    self.assertEqual(1, ts.countTestCases())
+
+    res = unittest.TestResult()
+    ts.run(res)
+    self.assertEqual(1, res.testsRun)
+    self.assertFalse(res.wasSuccessful())
+    self.assertIn('without having inherited', str(res.errors[0]))
+
+  def test_correct_extraction_numbers(self):
+    ts = unittest.makeSuite(self.GoodAdditionParams)
+    self.assertEqual(2, ts.countTestCases())
+
+  def test_successful_execution(self):
+    ts = unittest.makeSuite(self.GoodAdditionParams)
+
+    res = unittest.TestResult()
+    ts.run(res)
+    self.assertEqual(2, res.testsRun)
+    self.assertTrue(res.wasSuccessful())
+
+  def test_correct_arguments(self):
+    ts = unittest.makeSuite(self.GoodAdditionParams)
+    res = unittest.TestResult()
+
+    params = set([
+        (1, 2, 3),
+        (4, 5, 9)])
+    for test in ts:
+      test(res)
+      self.assertIn(test.arguments, params)
+      params.remove(test.arguments)
+    self.assertEmpty(params)
+
+  def test_recorded_failures(self):
+    ts = unittest.makeSuite(self.MixedAdditionParams)
+    self.assertEqual(2, ts.countTestCases())
+
+    res = unittest.TestResult()
+    ts.run(res)
+    self.assertEqual(2, res.testsRun)
+    self.assertFalse(res.wasSuccessful())
+    self.assertLen(res.failures, 1)
+    self.assertEmpty(res.errors)
+
+  def test_short_description(self):
+    ts = unittest.makeSuite(self.GoodAdditionParams)
+    short_desc = list(ts)[0].shortDescription()
+
+    location = unittest.util.strclass(self.GoodAdditionParams).replace(
+        '__main__.', '')
+    expected = ('{}.test_addition0 (1, 2, 3)\n'.format(location) +
+                'test_addition(1, 2, 3)')
+    self.assertEqual(expected, short_desc)
+
+  def test_short_description_addresses_removed(self):
+    ts = unittest.makeSuite(self.ArgumentsWithAddresses)
+    short_desc = list(ts)[0].shortDescription().split('\n')
+    self.assertEqual(
+        'test_something(<object>)', short_desc[1])
+    short_desc = list(ts)[1].shortDescription().split('\n')
+    self.assertEqual(
+        'test_something(<__main__.MyOwnClass>)', short_desc[1])
+
+  def test_id(self):
+    ts = unittest.makeSuite(self.ArgumentsWithAddresses)
+    self.assertEqual(
+        (unittest.util.strclass(self.ArgumentsWithAddresses) +
+         '.test_something0 (<object>)'),
+        list(ts)[0].id())
+    ts = unittest.makeSuite(self.GoodAdditionParams)
+    self.assertEqual(
+        (unittest.util.strclass(self.GoodAdditionParams) +
+         '.test_addition0 (1, 2, 3)'),
+        list(ts)[0].id())
+
+  def test_str(self):
+    ts = unittest.makeSuite(self.GoodAdditionParams)
+    test = list(ts)[0]
+
+    expected = 'test_addition0 (1, 2, 3) ({})'.format(
+        unittest.util.strclass(self.GoodAdditionParams))
+    self.assertEqual(expected, str(test))
+
+  def test_dict_parameters(self):
+    ts = unittest.makeSuite(self.DictionaryArguments)
+    res = unittest.TestResult()
+    ts.run(res)
+    self.assertEqual(2, res.testsRun)
+    self.assertTrue(res.wasSuccessful())
+
+  def test_no_parameterized_tests(self):
+    ts = unittest.makeSuite(self.NoParameterizedTests)
+    self.assertEqual(4, ts.countTestCases())
+    short_descs = [x.shortDescription() for x in list(ts)]
+    full_class_name = unittest.util.strclass(self.NoParameterizedTests)
+    full_class_name = full_class_name.replace('__main__.', '')
+    self.assertSameElements(
+        [
+            '{}.testGenerator'.format(full_class_name),
+            '{}.test_generator'.format(full_class_name),
+            '{}.testNormal'.format(full_class_name),
+            '{}.test_normal'.format(full_class_name),
+        ],
+        short_descs)
+
+  def test_successful_product_test_testgrid(self):
+
+    class GoodProductTestCase(parameterized.TestCase):
+
+      @parameterized.product(
+          num=(0, 20, 80),
+          modulo=(2, 4),
+          expected=(0,)
+      )
+      def testModuloResult(self, num, modulo, expected):
+        self.assertEqual(expected, num % modulo)
+
+    ts = unittest.makeSuite(GoodProductTestCase)
+    res = unittest.TestResult()
+    ts.run(res)
+    self.assertEqual(ts.countTestCases(), 6)
+    self.assertEqual(res.testsRun, 6)
+    self.assertTrue(res.wasSuccessful())
+
+  def test_successful_product_test_kwarg_seqs(self):
+
+    class GoodProductTestCase(parameterized.TestCase):
+
+      @parameterized.product((dict(num=0), dict(num=20), dict(num=0)),
+                             (dict(modulo=2), dict(modulo=4)),
+                             (dict(expected=0),))
+      def testModuloResult(self, num, modulo, expected):
+        self.assertEqual(expected, num % modulo)
+
+    ts = unittest.makeSuite(GoodProductTestCase)
+    res = unittest.TestResult()
+    ts.run(res)
+    self.assertEqual(ts.countTestCases(), 6)
