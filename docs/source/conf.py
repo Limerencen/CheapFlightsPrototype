9322fd3fe09500f6c1e6abaec90bf21bce6cbddb
Limerencen <105069436+Limerencen@users.noreply.github.com>
2023-02-12 18:24:45 +0000

    :globe_with_meridians: l10n updated

---
+++ docs/source/conf.py
@@ -0,0 +1,8 @@
+"""Sphinx config file for https://github.com/abseil/abseil-py."""
+
+import os
+import sys
+
+# -- Project information
+project = 'Abseil Python Common Libraries'
+copyright = '2022, Abseil'  # pylint: di
